0f14d3d890d8bf64a3c32f2779d8326f388c9321
Duy Dao <jimmydao@gmail.com>
2021-06-21 18:47:30 -0500

    Working on base

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
 console.log('Here are all the available people:', people);
+
+$(document)ready(onReady){
+  //need a listener
+  $().on('click',){
+    //run randomNumber first and then runPerson handler to see if it matches.
+  }
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+//need handler with if (match == number) alert You guessed right! else, You've guessed wrong! Try again!
+function pickPerson (){
+  if (){
+    alert(`You guessed right!`);
+  }
+  alert(`You've guessed wrong! Try again!`);
+}
--- data.js
+++ data.js
@@ -28,3 +28,23 @@ var people = [
     githubUsername: 'christopher-black'
   },
 ];
+
+
+$(document)ready(onReady){
+  //need a listener
+  $().on('click',){
+    //run randomNumber first and then runPerson handler to see if it matches.
+  }
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+//need handler with if (match == number) alert You guessed right! else, You've guessed wrong! Try again!
+function pickPerson (){
+  if (){
+    alert(`You guessed right!`);
+  }
+  alert(`You've guessed wrong! Try again!`);
+}
