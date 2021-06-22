bf54b6d4b27ca5dc8ebc181799b5756cfbc18471
Duy Dao <jimmydao@gmail.com>
2021-06-21 19:06:33 -0500

    Added gitHub links to data.js

--- data.js
+++ data.js
@@ -1,50 +1,44 @@
 var people = [
   {
+    name: 'Duy',
+    githubUsername: 'DuyDao-Dev'
+    //link to photo: https://avatars.githubusercontent.com/u/80065469?v=4
+  },
+  {
     name: 'Dev',
     githubUsername: 'devjanaprime'
+    //https://avatars.githubusercontent.com/u/17906158?v=4
   },
   {
     name: 'Dane',
     githubUsername: 'DoctorHowser'
+    //https://avatars.githubusercontent.com/u/11574995?v=4
   },
   {
     name: 'Mary',
     githubUsername: 'mbmosman'
+    //https://avatars.githubusercontent.com/u/5143491?v=4
   },
   {
     name: 'Kris',
     githubUsername: 'kdszafranski'
+    //https://avatars.githubusercontent.com/u/5541481?v=4
   },
   {
     name: 'Edan',
     githubUsername: 'eschwartz'
+    //https://avatars.githubusercontent.com/u/1153371?v=4
   },
   {
     name: 'Casie',
     githubUsername: 'cassandradanger'
+    //https://avatars.githubusercontent.com/u/10237149?v=4
   },
   {
     name: 'Chris',
     githubUsername: 'christopher-black'
+    //https://avatars.githubusercontent.com/u/25748592?v=4
   },
 ];
 
 
-$(document)ready(onReady){
-  //need a listener
-  $().on('click',){
-    //run randomNumber first and then runPerson handler to see if it matches.
-  }
-}
-
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
-
-//need handler with if (match == number) alert You guessed right! else, You've guessed wrong! Try again!
-function pickPerson (){
-  if (){
-    alert(`You guessed right!`);
-  }
-  alert(`You've guessed wrong! Try again!`);
-}
