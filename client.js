bc377f51f3a4659fef788014dbed4b2c4bc7a544
Duy Dao <jimmydao@gmail.com>
2021-06-23 20:33:01 -0500

    OMG Katie was such a big help!

--- client.js
+++ client.js
@@ -1,30 +1,35 @@
 console.log('Here are all the available people:', people);
 
-$(document).ready(onReady){
-  //need a listener
-  $('.primePhotos').on('click', {
-    //run randomNumber first and then runPerson handler to see if it matches.
+$(document).ready(function(){
+$('body').on('click', 'div', findMatch)
+appendPerson(); 
+randomPerson();
+})
+
+let namePicked;
+function randomPerson(){
+  let min = 0;
+ let max = people.length -1;
+ let answer =  Math.floor(Math.random() * (1 + max - min) + min);
+ namePicked = people[answer].name
+  return namePicked;
+}
+// console.log(randomPerson);
+// //need handler with if (match == number) alert You guessed right! else, You've guessed wrong! Try again!
+function appendPerson (){
+  for (const person of people) {
+   $('body').append(`
+<div data-id=${person.name}>
+        <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of"${person.name}">
+        </div>`)
   }
-}
-
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
-
-//need handler with if (match == number) alert You guessed right! else, You've guessed wrong! Try again!
-function pickPerson (userName){
-    const userName = ();
-    for (userName in employee) {
-        if (employee.gitHubUserName[].call()) {
-            const userName = gitHubUserName[key];
-            
-        }
-    }
 }
 
 function findMatch(){
-  if (pickPerson.userName === randomNumber.output){
-    alert(`You guessed right!`);
+  console.log();
+  let clickedName = $(this).data('id');
+  if (clickedName == namePicked){
+    alert(`You've got a match!`)
   }
-  alert(`You've guessed wrong! Try again!`);
+  else alert(`You've chose wrong! Try again!`)
 }
